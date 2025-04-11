f65a988642a74cbf10e05e5d4e1e68da846b6674
naimur-reza <naimur.rezaa@gmail.com>
2025-04-11 21:48:06 +0600

    Update: Validation Schema updated

---
+++ src/components/multi-step-form.tsx
@@ -0,0 +1,5 @@
+const MultiStepForm = () => {
+  return <></>;
+};
+
+export default MultiStepForm;
---
+++ src/schema/userSchema.ts
@@ -0,0 +1,18 @@
+import { z } from "zod";
+
+export const userSchema = z
+  .object({
+    name: z.string().min(1, "Name is required"),
+    email: z.string().email("Invalid email address"),
+    phone: z.string().optional(),
+    street: z.string().min(1, "Street is required"),
+    city: z.string().min(1, "City is required"),
+    state: z.string().min(1, "State is required"),
+    zip: z.string().min(1, "Zip code is required"),
+    userName: z.string().min(1, "Username is required"),
+    password: z.string().min(1, "Password is required"),
+    confirmPassword: z.string().min(1, "Confirm Password is required"),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords don't match",
+  });
