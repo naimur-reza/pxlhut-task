612aaf0b90ac7d10ce7f22f7b4266f56e7432819
naimur-reza <naimur.rezaa@gmail.com>
2025-04-11 21:22:29 +0600

    Add form validation with zod and enhance input component error handling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,36 @@
 
 import Form from "@/components/form/form";
 import Input from "@/components/form/input";
+import { FieldValues } from "react-hook-form";
+import { z } from "zod";
 
 const page = () => {
+  const schema = z.object({
+    name: z.string().min(2, "Name is required"),
+    email: z.string().email("Invalid email address"),
+    password: z.string().min(8, "Password must be at least 8 characters long"),
+  });
+
+  const handleSubmit = async (data: FieldValues) => {
+    const validationResult = schema.safeParse(data);
+    if (!validationResult.success) {
+      console.error(validationResult.error.format());
+      return;
+    }
+    console.log(data);
+  };
+  const defaultValues = {
+    name: "",
+    email: "",
+    password: "",
+  };
+
   return (
     <Form
       className="grid grid-cols-3 gap-5"
-      onSubmit={async (data) => console.log(data)}
+      schema={schema}
+      defaultValues={defaultValues}
+      onSubmit={handleSubmit}
     >
       <Input name="name" placeHolder="Name" />
       <Input name="email" placeHolder="Email" />
--- src/components/form/input.tsx
+++ src/components/form/input.tsx
@@ -15,12 +15,17 @@ const Input = ({
     <Controller
       control={control}
       name={name}
-      render={({ field }) => (
-        <input
-          className="w-full px-4 py-2.5 bg-white dark:bg-gray-950 border  dark:border-gray-700 rounded-xl shadow-sm focus:outline-none focus:ring focus:ring-white/20 focus:border-transparent placeholder-gray-400 dark:placeholder-gray-500 text-sm text-gray-900 dark:text-white transition"
-          {...field}
-          placeholder={placeHolder}
-        />
+      render={({ field, fieldState: { error } }) => (
+        <div>
+          <input
+            className="w-full px-4 py-2.5 bg-white dark:bg-gray-950 border  dark:border-gray-700 rounded-xl shadow-sm focus:outline-none focus:ring focus:ring-white/20 focus:border-transparent placeholder-gray-400 dark:placeholder-gray-500 text-sm text-gray-900 dark:text-white transition"
+            {...field}
+            placeholder={placeHolder}
+          />
+          {error && (
+            <span className="text-red-500/95 text-xs ">{error.message}</span>
+          )}
+        </div>
       )}
     />
   );
