50c0d929785bd70740b58d8838d0d77100c14117
naimur-reza <naimur.rezaa@gmail.com>
2025-04-12 17:21:15 +0600

    Refactor: Adjust layout of theme toggle for improved positioning

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,10 @@ import ThemeToggle from "@/components/theme-toggle";
 
 export default function Home() {
   return (
-    <main className="min-h-screen  flex items-center justify-center p-4 ">
-      <ThemeToggle />
+    <main className="min-h-screen relative flex items-center justify-center p-4 ">
+      <div className="absolute right-5 top-5">
+        <ThemeToggle />
+      </div>
       <div className="w-full max-w-2xl">
         <h1 className="text-3xl font-bold text-center mb-8">Registration</h1>
         <MultiStepForm />
